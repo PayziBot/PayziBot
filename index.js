cd5410918d497c74d0709a040bd576da4689f25c
Payziii <velmikhail123@gmail.com>
2026-07-02 22:20:39 +0500

    и тут

--- index.js
+++ index.js
@@ -37,8 +37,6 @@ client.textCommands = new Collection();
 client.textAliases = new Collection();
 client.cooldowns = new Collection();
 client.autoreactChannels = [];
-client.cmdsUsed = 0;
-client.cmdsDetailed = new Map();
 
 const eventsPath = path.join(__dirname, 'src', 'events');
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
