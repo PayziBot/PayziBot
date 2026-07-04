6730380a3073e054193a59d0077999bd25a449c9
Payziii <velmikhail123@gmail.com>
2026-07-04 13:22:24 +0500

    [ / ] Обновил API для получения данных о майнкрафт-сервере

--- src/func/apis/minecraft.js
+++ src/func/apis/minecraft.js
@@ -1,23 +1,20 @@
 async function getServer(ip) {
-    let answer;
-    await fetch(`https://api.mcsrvstat.us/3/${encodeURIComponent(ip)}`).then(r => r.json()).then(r => {
-        answer = r;
+    await fetch(`https://api.mcstatus.io/v2/status/java/${encodeURIComponent(ip)}`).then(r => r.json()).then(r => {
+        return r;
     }).catch(e => {
         console.log(e);
-        answer = 'error';
+        return 'error';
     });
-    return answer;
 }
 
 async function getPlayer(name) {
     let answer;
     await fetch(`https://playerdb.co/api/player/minecraft/${name}`).then(r => r.json()).then(r => {
-        answer = r;
+        return r;
     }).catch(e => {
         console.log(e);
-        answer = 'error';
+        return 'error';
     });
-    return answer;
 }
 
 
